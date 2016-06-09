1b0102a70b00b6f0d6e6e6f6a986af0a32476d36
horace <hp264@cornell.edu>
2016-06-08 19:55:58 -0500

    util

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import pdb
 import sys
 import json
 import urllib2
@@ -9,8 +10,34 @@ def yield_all_summaries(years, bill_nums, congress):
         for bill_num in range(1, num_bills+1):
             yield get_summary(congress, year, bill_num)
 
-def get_all_topics(years, bill_nums, congress):
-    pass
+
+def write_all_topics(years, bill_nums, congress, fname='data/all_topics.csv'):
+    f = open(fname, 'a')
+    all_topics = set()
+    for t in yield_all_topics(years, bill_nums, congress):
+        f.write(t+'\n')
+    print("Done")
+
+def yield_all_topics(years, bill_nums, congress):
+    topic_set = set()
+    for i in range(len(years)):
+        num_bills = bill_nums[i]
+        year = years[i]
+        for bill_num in range(1, num_bills+1):
+            t = get_topic(congress, year, bill_num)
+            yield(t)
+            topic_set.add(t)
+            print("Done with %d/%d" %(year, bill_num))
+
+def get_topic(congress, year, vote_num):
+    vote_json = get_vote_json(congress, year, vote_num)
+    if vote_json['category'] == 'nomination':
+        return 'Nomination'
+    else:
+        bill_type = vote_json['bill']['type']
+        bill_num = int(vote_json['bill']['number'])
+        bill_json = get_bill_json(congress, bill_type, bill_num)
+        return bill_json['subjects_top_term']
 
 def get_summary(congress, year, vote_num):
     vote_json = get_vote_json(congress, year, vote_num)
@@ -27,21 +54,9 @@ def get_summary(congress, year, vote_num):
         bill_json = get_bill_json(congress, bill_type, bill_num)
         return bill_json['summary']['text']
 
-def get_topic(congress, year, vote_num):
-    vote_json = get_vote_json(congress, year, vote_num)
-    if vote_json['category'] == 'nomination':
-        return vote_json['question']
-    else:
-        bill_type = vote_json['bill']['type']
-        bill_num = int(vote_json['bill']['number'])
-        bill_json = get_bill_json(congress, bill_type, bill_num)
-        return bill_json['summary']['text']
-
-
 def get_vote_json(congress, year, bill_num):
     url = 'https://www.govtrack.us/data/congress/%d/votes/%d/s%d/data.json' \
           %(congress, year, bill_num)
-    print(url)
     vote_json = json.load(urllib2.urlopen(url))
     return vote_json
 
@@ -54,7 +69,6 @@ def get_amendment_json(congress, amend_type, amend_num):
 def get_bill_json(congress, bill_type, bill_num):
     url = 'https://www.govtrack.us/data/congress/%d/bills/%s/%s%d/data.json' \
           %(congress, bill_type, bill_type, bill_num)
-    print(url, 'in get bill json')
     bill_json = json.load(urllib2.urlopen(url))
     return bill_json
 
@@ -62,4 +76,4 @@ def get_sum_bill(bill_json):
     pass
 
 if __name__ == '__main__':
-    print(get_summary(114, 2016, int(sys.argv[1])))
+    write_all_topics([2015, 2016], [339,88], 114)
